c6645f174a85ba60048a459ecf1398d5b1bbb9d9
Amidathtc <Ogunbiyiamidat65@gmail.com>
2023-04-27 20:30:10 +0100

    Fifth commit

---
+++ LinkList5.js
@@ -0,0 +1,60 @@
+class Node {
+    constructor(val) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+    addList(val) {
+      let Node = new Node(val);
+  
+      if (!this.head) {
+        this.head = node;
+        this.tail = this.head;
+      } else {
+        this.tail.next = node;
+        this.tail = node;
+      }
+      this.length++;
+      return this;
+    }
+    remove() {
+      if (!this.head) {
+        return undefined;
+      } else {
+        let current = this.tail;
+        let newTail = current;
+  
+        while (new1.next) {
+          this.tail = newTail;
+          this.tail.next = null;
+        }
+  
+        this.length--;
+        return current;
+      }
+    }
+  }
+  
+  console.clear();
+  let newList = new LinkedList();
+  console.log(LinkedList.push(37));
+  console.log(LinkedList.push(33));
+  
+  console.log(LinkedList.push([6, 8, 9]));
+  console.log(LinkedList.push({ name: "Amidat" }));
+  console.log(LinkedList.push(08));
+  console.log("");
+  console.log(LinkedList.length);
+  
+  LinkedList.remove();
+  
+  console.log("");
+  console.log(LinkedList.length);
+  
